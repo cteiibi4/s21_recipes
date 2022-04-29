c847025a17034f106c64128a27ea03e2c1bfe48b
steapn-v <steapn-v@paycon.su>
2022-04-29 17:37:35 +0500

    Созданы основные структуры в бд

---
+++ common.py
@@ -0,0 +1,1 @@
+BASE = 'Enginetech_Catalog.db'
---
+++ init_db.py
@@ -0,0 +1,34 @@
+from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey, Date, create_engine
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+from common import BASE
+
+engine = create_engine(f'sqlite:///{BASE}', echo=True)
+
+Base = declarative_base()
+
+
+class Recipe(Base):
+    __tablename__ = 'recipes'
+    id = Column('id', Integer, primary_key=True, unique=True)
+    name = Column('name', String)
+    date = Column('date', Date)
+    images = relationship('Image', back_populates='recipes')
+
+    def __init__(self, name, date):
+        self.name = name
+        self.date = date
+
+
+class Image(Base):
+    __tablename__ = 'images'
+    id = Column(Integer, primary_key=True)
+    image = Column(String)
+    recipe = relationship('Recipe', back_populates='images')
+    recipe_id = Column(String, ForeignKey('recipes.id'))
+
+    def __init__(self, image):
+        self.image = image
+
+
+Base.metadata.create_all(engine)
